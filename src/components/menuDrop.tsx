d230b1a25f152bd1902cc2f5c658f879ee31d92b
vinicius <viniciuseduardorr@gmail.com>
2024-11-05 12:16:52 -0300

    feat(Menu dropdown) - Adicionando menu flutuante com animaçoes e adicionando a anim do botao

---
+++ src/components/menuDrop.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface MenuDropProps {
+  isOpen: boolean;
+}
+
+const MenuDrop: React.FC<MenuDropProps> = ({ isOpen }) => {
+  return (
+    <div
+      className={` absolute top-16 right-[19px] mt-2 w-80 h-72 rounded-3xl bg-neutral-100 shadow-customCardShadow z-40 transform origin-top transition-all duration-300 ease-in-out ${
+        isOpen ? 'scale-y-100 opacity-100 pointer-events-auto' : 'scale-y-0 opacity-0 pointer-events-none'
+      }`}
+    >
+      <div className="bg-neutral-100 w-4 h-4 rotate-45 absolute right-3 transform -translate-x-1/2 -top-1 z-50">
+      </div>
+      <ul className="p-4 flex flex-col space-y-4 items-center ">
+        <li className="py-1 w-40 text-center border-b-2 hover:bg-gray-200 rounded-md transition-all duration-200 ease-in-out">Recurring Payment</li>
+        <li className="py-1 w-40 text-center border-b-2 hover:bg-gray-200 rounded-md transition-all duration-200 ease-in-out">Reminder</li>
+        <li className="py-1 w-40 text-center border-b-2 hover:bg-gray-200 rounded-md transition-all duration-200 ease-in-out">Charts</li>
+        <li className="py-1 w-40 text-center border-b-2 hover:bg-gray-200 rounded-md transition-all duration-200 ease-in-out">Categories</li>
+        <li className="py-1 w-40 text-center border-b-2 hover:bg-gray-200 rounded-md transition-all duration-200 ease-in-out">Payment Method</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MenuDrop;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,21 @@
-import { Wallet, Minus} from "lucide-react";
 import ReactECharts from 'echarts-for-react';
 import { EChartsOption } from 'echarts';
 
 import { useColor } from '../hooks/useColor';
 import CardLayout from "../components/cardlayout";
 import TransactionForm from "../components/transactionForm";
+import { useState } from "react";
+import MenuDrop from '../components/menuDrop';
 
 
 
 export function MainPage() {
   const { colors, loading } = useColor();
-
-
-
-
+  const [isOpen, setIsOpen] = useState(false);
 
   if (loading) {
     return <div>Loading...</div>;
   }
-
 
   const data = [
     { value: 1048, name: 'Manutenção', itemStyle: { color: colors['3ed1d80b-c0e4-44dd-a1c8-97f222a6a1c6'] } },
@@ -65,17 +62,26 @@ export function MainPage() {
     ]
   };
 
-
-
   return (
     <div className="h-screen max-h-screen overflow-hidden bg-slate-100 ">
       <div className="flex flex-col h-full">
         <div className="bg-neutral-400 h-16 w-full px-8 py-2 flex justify-between items-center">
-          <Wallet />
-          <span className="flex flex-col">
-            <Minus className="relative top-1.5" />
-            <Minus className="relative bottom-1.5" />
+          <img src="src/assets/icon.svg" />
+          <span className="flex flex-col cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+          <span className="flex flex-col ">
+            <span
+              className={`block w-8 h-0.5 bg-current transition-transform duration-500 ease-in-out ${
+                isOpen ? 'rotate-45 translate-y-2.5' : 'rotate-0 translate-y-0'
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-0.5 bg-current mt-3 transition-transform duration-500 ease-in-out ${
+                isOpen ? '-rotate-[405deg] -translate-y-1' : 'rotate-0 translate-y-0'
+              }`}
+            ></span>
           </span>
+          <MenuDrop isOpen={isOpen} />
+        </span>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="absolute top-12">
           <path fill="#a3a3a3" fillOpacity="1" d="M0,24L26.7,32C53.3,40,107,56,160,54C213.3,52,267,32,320,32C373.3,32,427,52,480,50.7C533.3,49,587,24,640,28C693.3,32,747,64,800,84C853.3,104,907,112,960,108C1013.3,104,1067,88,1120,73.3C1173.3,60,1227,48,1280,50.7C1333.3,52,1387,68,1413,76L1440,80L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path>
